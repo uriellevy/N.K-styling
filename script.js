72c5ba7fb7a6c65ac945f9fda736b259207e8ab6
uriel levy <uriellevy4892@gmail.com>
2021-09-22 14:42:37 +0300

    header

---
+++ script.js
@@ -0,0 +1,14 @@
+////navbar
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
+
+/////sticky navbar
+const nav2 = document.querySelector(".full-nav");
+window.addEventListener("scroll", function () {
+  nav2.classList.toggle("sticky", window.scrollY > 900);
+});
