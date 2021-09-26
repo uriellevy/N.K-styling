be63825a1887a8f0427e79fca40f9b691a318e04
uriel levy <uriellevy4892@gmail.com>
2021-09-26 22:33:00 +0300

    gallery

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ const nav2 = document.querySelector(".full-nav");
 window.addEventListener("scroll", function () {
   nav2.classList.toggle("sticky", window.scrollY > 600);
 });
+
+// /////GALLERY
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+//set first img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach((img) => {
+  img.addEventListener("click", imgClick);
+});
+
+function imgClick(e) {
+  //reset all imgs opacity
+  imgs.forEach((img) => {
+    img.style.opacity = 1;
+  });
+  //change img to src img
+  current.src = e.target.src;
+
+  //add fadeIn class
+  current.classList.add("fade-in");
+  //remove fadein class after 0.5s
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+  //change opacity
+  e.target.style.opacity = opacity;
+}
